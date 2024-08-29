eb3871d50296474638b9e53bc9c07a4f45c3e955
Ansh <anshgandharve@gmail.com>
2024-08-29 21:17:50 +0530

    Car Dealership

---
+++ src/Customer.java
@@ -0,0 +1,51 @@
+public class Customer {
+
+    private String name;
+    private String address;
+    private float cashOnHand;
+
+    public Customer(String name, String address, float cashOnHand) {
+        this.name = name;
+        this.address = address;
+        this.cashOnHand = cashOnHand;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+
+    public String getAddress() {
+        return address;
+    }
+
+//    public void setAddress(String address) {
+//        this.address = address;
+//    }
+
+    public float getCashOnHand() {
+        cashOnHand += 500;
+        return cashOnHand;
+    }
+
+//    public void setCashOnHand(float cashOnHand) {
+//        cashOnHand += 500;
+//        this.cashOnHand = cashOnHand;
+//    }
+
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "Name='" + name + '\'' +
+                ", Address='" + address + '\'' +
+                '}';
+    }
+
+    public void purchaseCar(Vehicle vehicle, Employee emp, boolean finance){
+        emp.handleCustomer(this,vehicle,finance);
+    }
+}
---
+++ src/Vehicle.java
@@ -0,0 +1,47 @@
+public class Vehicle {
+    private String vehBrand;
+    private String vehModel;
+    private int vehPrice;
+
+
+    public Vehicle(String vehBrand, String vehModel, int vehPrice) {
+        this.vehBrand = vehBrand;
+        this.vehModel = vehModel;
+        this.vehPrice = vehPrice;
+    }
+
+    public String getVehBrand() {
+        return vehBrand;
+    }
+
+    public void setVehBrand(String vehBrand) {
+        this.vehBrand = vehBrand;
+    }
+
+
+
+    public String getVehModel() {
+        return vehModel;
+    }
+
+    public void setVehModel(String vehModel) {
+        this.vehModel = vehModel;
+    }
+
+    public void setVehPrice(int vehPrice) {
+        this.vehPrice = vehPrice;
+    }
+
+    public int getVehPrice() {
+        return vehPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "Brand='" + vehBrand + '\'' +
+                ", Model='" + vehModel + '\'' +
+                ", Price=" + vehPrice +
+                '}';
+    }
+}
